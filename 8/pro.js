621db723d15164d5d5d208c6dc854d904f697512
Ferdi Sefa Düzgün <76045185+ferdisefaduzgun@users.noreply.github.com>
2023-06-02 18:47:55 +0300

    Add files via upload

---
+++ 8/pro.js
@@ -0,0 +1,60 @@
+let isRaining = false
+
+if (isRaining) {
+    console.log('evet yağmur yağıyor');
+}else{
+    console.log('hava güneşli');
+}
+
+
+/* let soru = prompt('2 + 5 kaç yapar?', '2+5')
+
+if(soru == '7'){
+    console.log('dogru cevap bravo');
+}else{
+    console.log('yanlış, cevap 7 olmalı');
+} */
+
+
+
+/* let number = prompt('3 + 2 kaç yapar', '3 + 2');
+
+if(number == 4){
+    console.log('4se buraya yonlendır');
+}else if(number == 7){
+    console.log('7yse boyle yap');
+}else if(number == 5){
+    console.log('dogru cvp');
+}
+else{
+    console.log('bulamadin');
+} */
+
+
+/* switch if in alternatifidir . 
+*/
+
+let number2 = 7
+
+switch(number2){
+    case  7: 
+    console.log('dogru');
+    break
+
+    default:
+        console.log('default');
+}
+/* 
+switch(caseValue){    
+    case 1:
+      // code
+      break             bu kod dogruysa aşağıdakileri okumasın diye break koyariz
+    case 2:
+     // code
+     break
+    case 3:
+     // code
+     break
+    default:
+     // code
+  } */
