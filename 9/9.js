d64f906e58c4e5f7b6272ec450c0aef7063e0a03
Ferdi Sefa Düzgün <76045185+ferdisefaduzgun@users.noreply.github.com>
2023-06-03 12:41:18 +0300

    Add files via upload

---
+++ 9/9.js
@@ -0,0 +1,13 @@
+let youtube = "sefaduzgun";
+
+let result;
+
+result = youtube.toLowerCase();
+result = youtube.toUpperCase();
+result = youtube.length();
+result = youtube.trim();
+result = youtube.slice(0,6);
+result = youtube.split("");
+result = youtube[0];
+
+console.log(result);
